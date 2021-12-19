039c06705517462f626a0a8227bba1a5d6d6661b
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-19 20:29:17 +0800

    added api to fetch all heroes

--- routes/heroes/heroes.js
+++ routes/heroes/heroes.js
@@ -3,11 +3,24 @@ const router = express.Router();
 const pool = require("../../db");
 const cors = require("cors");
 
+router.get("/getAllHeroes", cors(), async (req, res) => {
+  try {
+    const r = await pool.query(`
+            SELECT encode(h.image_url, 'escape') AS image_url, h.hero_name, h.hero_id
+            FROM heroes h
+        `);
+    const result = await r.rows;
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get("/getHeroById/:id", cors(), async (req, res) => {
   try {
     const id = req.params.id;
-
-    const rows = await pool.query(`SELECT 
+    const rows = await pool.query(
+      `SELECT 
         encode(h.image_url, 'escape') as image_url,
         h.hero_name,
         h.hero_id,
@@ -34,9 +47,9 @@ router.get("/getHeroById/:id", cors(), async (req, res) => {
         JOIN countries co ON co.country_id = h.origin_country
         JOIN team t ON t.team_id = h.team_id
         JOIN faction f ON t.faction_id = f.faction_id
-        WHERE hero_id = $1`, [
-      id,
-    ]);
+        WHERE hero_id = $1`,
+      [id]
+    );
     const response = rows.rows[0];
     res.json(response);
   } catch (error) {
