038fee693b83c601b024512e12a32cf75cebb938
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-24 23:59:05 +0800

    updated categories dialog

--- client/src/components/settings/categories-details/categories-details-dialog.js
+++ client/src/components/settings/categories-details/categories-details-dialog.js
@@ -1,54 +1,60 @@
-import { Dialog, DialogActions, DialogContent, Button, DialogTitle, TextField } from "@mui/material";
-import { useFormik } from 'formik';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Button,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useFormik } from "formik";
 
 export function CategoriesDetailsDialog({ openDialog, setOpenDialog, data }) {
+  var formik;
 
-    var formik;
+  console.log(data);
 
-    const submitCategory = (val) => {
-        console.log("submitted val: ", val);
-    }
+  const submitCategory = (val) => {
+    console.log("submitted val: ", val);
+  };
 
-    const GetFormik = () => {
-        formik = useFormik({
-            initialValues: {
-                category_name: data.category_name
-            },
-            onSubmit: x => {
-                submitCategory(x);
-            }, enableReinitialize: true
-        });
-    }
+  const GetFormik = () => {
+    formik = useFormik({
+      initialValues: {
+        category_name: data.category_name,
+      },
+      onSubmit: (x) => {
+        submitCategory(x);
+      },
+      enableReinitialize: true,
+    });
+  };
 
-    if (data) {
-        GetFormik();
-        return (
-            <Dialog
-                open={openDialog}>
-                <DialogTitle>
-                    Edit Category
-                </DialogTitle>
-                <form onSubmit={formik.handleSubmit}>
-                    <DialogContent>
-                        <div className="mt-2">
-                            <TextField
-                                label="Category Name"
-                                type="text"
-                                margin="dense"
-                                onChange={formik.handleChange}
-                                value={formik.values.category_name}
-                                name="category_name" />
-                        </div>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={() => setOpenDialog(false)}>DISMISS</Button>
-                        <Button type="submit">SAVE</Button>
-                    </DialogActions>
-                </form>
-            </Dialog >
-        )
-    }
-    else {
-        return (<div></div>);
-    }
+  if (data) {
+    GetFormik();
+    return (
+      <Dialog open={openDialog}>
+        <DialogTitle>{data.category_id ? "Edit" : "Add"} Category</DialogTitle>
+        <form onSubmit={formik.handleSubmit}>
+          <DialogContent>
+            <div className="mt-2">
+              <TextField
+                label="Category Name"
+                type="text"
+                margin="dense"
+                onChange={formik.handleChange}
+                value={formik.values.category_name}
+                name="category_name"
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenDialog(false)}>DISMISS</Button>
+            <Button type="submit">SAVE</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    );
+  } else {
+    return <div></div>;
+  }
 }
