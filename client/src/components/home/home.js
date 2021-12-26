1d95016a8f0b2e6c330bb239172d4c439a053fcc
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-26 21:21:52 +0800

    update for mobile view

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,53 +1,123 @@
 import "./home.css";
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Flex from "@react-css/flex";
+import Icon from "@mdi/react";
+import {
+  mdiCoffee,
+  mdiCubeOutline,
+  mdiFactory,
+  mdiGroup,
+  mdiMicroscope,
+  mdiPlus,
+  mdiShield,
+} from "@mdi/js";
 
 const Home = ({ setAuth, username }) => {
+  const [menuMobile] = useState([
+    {
+      title: "Cafe",
+      icon: mdiCoffee,
+    },
+    {
+      title: "Infirmary",
+      icon: mdiPlus,
+    },
+    {
+      title: "Research",
+      icon: mdiMicroscope,
+    },
+    {
+      title: "Factory",
+      icon: mdiCubeOutline,
+    },
+  ]);
   return (
     <Fragment>
       <div className="base-background">
-        <div className="character-container">
-          <img src="https://iopwiki.com/images/3/30/UMP45_costume4.png" />
-        </div>
-        <div className="home-menu-container">
-          <Flex
-            column
-            gap={10}
-            className="home-menu-container-item"
-            style={{ right: "0" }}
-          >
-            <Flex row className="subcontainer-item">
-              <div>
-                <h2>Cafe</h2>
-              </div>
-            </Flex>
-            <Flex row gap={5} className="subcontainer-item">
-              <div>
-                <h2>Research</h2>
-              </div>
-              <div>
-                <h2>Factory</h2>
-              </div>
-            </Flex>
-            <Flex className="w-100">
+        {window.innerWidth > 1000 ? (
+          <div>
+            <div className="character-container">
+              <img src="https://iopwiki.com/images/3/30/UMP45_costume4.png" />
+            </div>
+            <div className="home-menu-container">
               <Flex
-                rowReverse
-                gap={5}
-                className="subcontainer-item-bottom w-100"
+                column
+                gap={10}
+                className="home-menu-container-item"
+                style={{ right: "0" }}
               >
-                <div>
-                  <h2>Battle</h2>
-                </div>
-                <div>
-                  <h2>Formation</h2>
-                </div>
-                <div>
-                  <h2>H</h2>
-                </div>
+                <Flex row className="subcontainer-item">
+                  <div>
+                    <Flex row gap={5}>
+                      <Icon path={mdiCubeOutline} size={4} />
+                      <h2 className="mt-3">Cafe</h2>
+                    </Flex>
+                  </div>
+                </Flex>
+                <Flex row gap={5} className="subcontainer-item">
+                  <div>
+                    <Flex row gap={5}>
+                      <Icon path={mdiMicroscope} size={2} />
+                      <h2 className="mt-2">Research</h2>
+                    </Flex>
+                  </div>
+                  <div>
+                    <Flex row gap={5}>
+                      <Icon path={mdiFactory} size={2} />
+                      <h2 className="mt-2">Factory</h2>
+                    </Flex>
+                  </div>
+                </Flex>
+                <Flex className="w-100">
+                  <Flex
+                    rowReverse
+                    gap={5}
+                    className="subcontainer-item-bottom w-100"
+                  >
+                    <div>
+                      <Flex row gap={5}>
+                        <Icon path={mdiPlus} size={2} />
+                        <h2 className="mt-2">Infirmary</h2>
+                      </Flex>
+                    </div>
+                    <div>
+                      <Flex row gap={5}>
+                        <Icon path={mdiGroup} size={2} />
+                        <h2 className="mt-2">Formation</h2>
+                      </Flex>
+                    </div>
+                    <div>
+                      <Flex row gap={5}>
+                        <Icon path={mdiShield} size={2} />
+                        <h2 className="mt-2">Battle</h2>
+                      </Flex>
+                    </div>
+                  </Flex>
+                </Flex>
               </Flex>
+            </div>
+          </div>
+        ) : (
+          <div className="w-100">
+            <Flex
+              column
+              className=" mt-3 mobile-menu-base"
+              gap={10}
+              style={{ height: "10rem" }}
+            >
+              {menuMobile.map((x) => {
+                return (
+                  <div className="mobile-menu" key={x.title}>
+                    <Flex row>
+                      <Icon path={x.icon} size={1.5} />
+                      <h2>{x.title}</h2>
+                    </Flex>
+                  </div>
+                );
+              })}
             </Flex>
-          </Flex>
-        </div>
+          </div>
+        )}
       </div>
     </Fragment>
   );
--- client/src/footer.js
+++ client/src/footer.js
@@ -6,47 +6,68 @@ import "./App.css";
 function Footer() {
   return (
     <div className="footer w-100">
-      <div className="grid-container">
-        <div className="item1">
-          <Flex row>
-            <h3>Girls Frontline</h3>
-          </Flex>
-          <p>
-            Girls Frontline App is a community website built to assist players
-            by providing a guide for in-game dolls, weapons and items.
-          </p>
-          <div>
-            <strong>Developed by: JakeSiewJK64</strong>
-            <sup>
-              <strong className="rounded-pill m-1 bg-danger text-white p-1">
-                Ω
-              </strong>
-            </sup>
+      {window.innerWidth > 1000 ? (
+        <div className="grid-container">
+          <div className="item1">
+            <Flex row>
+              <h3>Girls Frontline</h3>
+            </Flex>
+            <p>
+              Girls Frontline App is a community website built to assist players
+              by providing a guide for in-game dolls, weapons and items.
+            </p>
+            <div>
+              <strong>Developed by: JakeSiewJK64</strong>
+              <sup>
+                <strong className="rounded-pill m-1 bg-danger text-white p-1">
+                  Ω
+                </strong>
+              </sup>
+            </div>
+          </div>
+          <div className="">
+            <Flex row>
+              <h3>Links</h3>
+            </Flex>
+            <Flex column>
+              <Flex row className="link">
+                <Icon path={mdiGithub} font={1} />
+                <a href="https://github.com/JakeSiewJK64/">Github</a>
+              </Flex>
+            </Flex>
+          </div>
+          <div style={{ height: "6rem" }}>
+            <Flex row>
+              <h3>Socials</h3>
+            </Flex>
+            <Flex column>
+              <Flex row className="link">
+                <Icon path={mdiDiscord} font={1} />
+                <a href="#">Discord</a>
+              </Flex>
+            </Flex>
           </div>
         </div>
-        <div className="">
-          <Flex row>
-            <h3>Links</h3>
-          </Flex>
-          <Flex column>
-            <Flex row className="link">
-              <Icon path={mdiGithub} font={1} />
-              <a href="https://github.com/JakeSiewJK64/">Github</a>
-            </Flex>
-          </Flex>
-        </div>
-        <div className="">
-          <Flex row>
-            <h3>Socials</h3>
-          </Flex>
-          <Flex column>
-            <Flex row className="link">
-              <Icon path={mdiDiscord} font={1} />
+      ) : (
+        <footer>
+          <Flex column className="mobile-footer pt-4">
+            <div className="bg-black w-75 rounded-pill p-2 mx-auto">
+              <h2>Socials</h2>
+            </div>
+            <Flex row className="link-style mx-auto m-2" alignItemsCenter>
+              <Icon path={mdiDiscord} font={1} style={{ width: "2rem" }} />
               <a href="#">Discord</a>
             </Flex>
+            <div className="bg-black w-75 rounded-pill p-2 mx-auto">
+              <h2>Links</h2>
+            </div>
+            <Flex row className="link-style mx-auto m-2" alignItemsCenter>
+              <Icon path={mdiGithub} font={1} style={{ width: "2rem" }} />
+              <a href="https://github.com/JakeSiewJK64">Github</a>
+            </Flex>
           </Flex>
-        </div>
-      </div>
+        </footer>
+      )}
     </div>
   );
 }
